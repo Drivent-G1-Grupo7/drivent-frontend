ad73b9044193e5d460abb9fc65e4dfdb745727c0
duanzin <lucaduan@gmail.com>
2023-05-11 16:35:08 -0300

    feat: renders button when online option is chosen

--- src/components/TicketSelection/TicketSelectionDiv.js
+++ src/components/TicketSelection/TicketSelectionDiv.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const TicketSelectionDiv = styled.div`
+  display: flex;
+  flex-direction: column;
   row-gap: 17px;
   margin-bottom: 44px;
   h2{
@@ -9,48 +11,8 @@ export const TicketSelectionDiv = styled.div`
     font-size: 20px;
     line-height: 23.44px;
     color: #8E8E8E;
-    margin-bottom: 17px;
-  };
-  label{
-    position: relative;
-    input{
-      appearance: none;
-      cursor: pointer;
-      width: 145px;
-      height: 145px;
-      border: 1px solid #CECECE;
-      border-radius: 20px;
-    };
-    input:checked {
-      background: #FFEED2;
-      border: none;
-      border-radius: 20px;
+    b{
+      font-weight: 700;
     }
-    div{
-      display: flex;
-      flex-direction: column;
-      position: absolute;
-      left: 0; 
-      right: 0; 
-      bottom: 58px;
-      margin-left: auto; 
-      margin-right: auto;
-      width: auto;
-      cursor: pointer;
-      span{
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 19px;
-        text-align: center;
-        color: #454545;
-        :last-child{
-          font-size: 14px;
-          line-height: 16px;
-          color: #898989;
-        }
-      }
-    }
-  };
+  }
 `;
--- src/components/TicketSelection/index.js
+++ src/components/TicketSelection/index.js
@@ -1,24 +1,69 @@
+import { useState, useEffect } from 'react';
 import { TicketSelectionDiv } from './TicketSelectionDiv';
+import { RadioInputDiv } from './RadioInputdiv';
+import { StyledButton } from './StyledButton';
+import { ChosenTicketDiv } from './ChosenTicketDiv';
 
 export default function TicketTypeSelection() {
+  const [value, setValue] = useState(0);
+  const [type, setType] = useState('');
+  const [showRadioInput, setShowRadioInput] = useState(true);
+  const [showBookTicketButton, setShowBookTicketButton] = useState(false);
+
+  const onTicketTypeChange = e => {
+    setValue(parseInt(e.target.value));
+    if (parseInt(e.target.value) === 100) {
+      setShowBookTicketButton(true);
+      setType('Online');
+    }
+    else{
+      setShowBookTicketButton(false);
+    }
+    console.log(e.target.value);
+  };
+
+  function handleClick() {
+    setShowRadioInput(false);
+    setShowBookTicketButton(false);
+  }
+
   return(
-    <TicketSelectionDiv>
-      <h2>Primeiro, escolha sua modalidade de ingresso</h2>
-      <label htmlFor="presencial">
-        <input type="radio" id="presencial" name="ingresso" value="presencial"/>
-        <div>
-          <span>Presencial</span>
-          <span>R$ 250</span>
-        </div>
-      </label>
-      <label htmlFor="online">
-        <input type="radio" id="online" name="ingresso" value="online"/>
-        <div>
-          <span>Online</span>
-          <span>R$ 100</span>
-        </div>
-      </label>
-    </TicketSelectionDiv>
+    <>{showRadioInput 
+      ?<TicketSelectionDiv>
+        <h2>Primeiro, escolha sua modalidade de ingresso</h2>
+        <RadioInputDiv>
+          <label htmlFor="presencial">
+            <input type="radio" id="presencial" name="ingresso" value="250" onChange={onTicketTypeChange}/>
+            <div>
+              <span>Presencial</span>
+              <span>R$ 250</span>
+            </div>
+          </label>
+          <label htmlFor="online">
+            <input type="radio" id="online" name="ingresso" value="100" onChange={onTicketTypeChange}/>
+            <div>
+              <span>Online</span>
+              <span>R$ 100</span>
+            </div>
+          </label>
+        </RadioInputDiv>
+      </TicketSelectionDiv>
+      :<TicketSelectionDiv>
+        <h2>Ingresso escolhido</h2>
+        <ChosenTicketDiv>
+          <span>{type}</span>
+          <span>R$ {value}</span>
+        </ChosenTicketDiv>
+      </TicketSelectionDiv>
+    }
+    {showBookTicketButton 
+      ?<TicketSelectionDiv>
+        <h2>Fechado! O total ficou em <b>R$ {value}.</b> Agora é só confirmar:</h2>
+        <StyledButton onClick={handleClick}>RESERVAR INGRESSO</StyledButton>
+      </TicketSelectionDiv> 
+      : <></>
+    }
+    </>
   );
 }
 
