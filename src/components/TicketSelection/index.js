7a5e5a7cdd6d8792702ac721c534e5bdf9c89044
DFCruzz <diegofcruzz@gmail.com>
2023-05-12 15:54:30 -0300

    feat: added hotel and no hotel input boxes

--- src/components/TicketSelection/index.js
+++ src/components/TicketSelection/index.js
@@ -5,21 +5,47 @@ import { StyledButton } from './StyledButton';
 import { ChosenTicketDiv } from './ChosenTicketDiv';
 
 export default function TicketTypeSelection() {
-  const [value, setValue] = useState(0);
-  const [type, setType] = useState('');
+  const [ticketValue, setTicketValue] = useState(0);
+  const [ticketType, setTicketType] = useState('');
+  const [bookingType, setBookingType] = useState('');
+  const [bookingValue, setBookingValue] = useState(0);
   const [showRadioInput, setShowRadioInput] = useState(true);
   const [showBookTicketButton, setShowBookTicketButton] = useState(false);
 
-  const onTicketTypeChange = e => {
+  /*const onTicketTypeChange = e => {
     setValue(parseInt(e.target.value));
     switch (parseInt(e.target.value)) {
-    case 100:
+      case 100:
+        setShowBookTicketButton(true);
+        setType('Online');
+        break;
+      default:
+        setShowBookTicketButton(false);
+        break;
+    }
+  };*/
+
+  const onTicketTypeChange = e => {
+    setTicketValue(parseInt(e.target.value));
+
+    if (ticketValue === '250') {
+      setTicketType('Presencial');
+    } else {
+      setTicketType('Online');
       setShowBookTicketButton(true);
-      setType('Online');
-      break;
-    default:
-      setShowBookTicketButton(false);
-      break;
+    }
+  };
+
+  const onBookingChange = e => {
+    setBookingValue(parseInt(e.target.value));
+    setShowBookTicketButton(false);
+
+    if (bookingValue === 0) {
+      setBookingType('Sem Hotel');
+      setShowBookTicketButton(true);
+    } else {
+      setBookingType('Com Hotel');
+      setShowBookTicketButton(true);
     }
   };
 
@@ -29,44 +55,65 @@ export default function TicketTypeSelection() {
     setShowBookTicketButton(false);
   };
 
-  return(
+  return (
     <form onSubmit={handleSubmit}>
-      {showRadioInput 
-        ?<TicketSelectionDiv>
-          <h2>Primeiro, escolha sua modalidade de ingresso</h2>
-          <RadioInputDiv>
-            <label htmlFor="presencial">
-              <input type="radio" id="presencial" name="ingresso" value="250" onChange={onTicketTypeChange}/>
-              <div>
-                <span>Presencial</span>
-                <span>R$ 250</span>
-              </div>
-            </label>
-            <label htmlFor="online">
-              <input type="radio" id="online" name="ingresso" value="100" onChange={onTicketTypeChange}/>
-              <div>
-                <span>Online</span>
-                <span>R$ 100</span>
-              </div>
-            </label>
-          </RadioInputDiv>
-        </TicketSelectionDiv>
-        :<TicketSelectionDiv>
+      {showRadioInput
+        ? <>
+          <TicketSelectionDiv>
+            <h2>Primeiro, escolha sua modalidade de ingresso</h2>
+            <RadioInputDiv>
+              <label htmlFor="presencial">
+                <input type="radio" id="presencial" name="ingresso" value="250" onChange={onTicketTypeChange} />
+                <div>
+                  <span>Presencial</span>
+                  <span>R$ 250</span>
+                </div>
+              </label>
+              <label htmlFor="online">
+                <input type="radio" id="online" name="ingresso" value="100" onChange={onTicketTypeChange} />
+                <div>
+                  <span>Online</span>
+                  <span>R$ 100</span>
+                </div>
+              </label>
+            </RadioInputDiv>
+          </TicketSelectionDiv>
+          {ticketType === 'Presencial' && (
+            <TicketSelectionDiv>
+              <h2>Primeiro, escolha sua modalidade de ingresso</h2>
+              <RadioInputDiv>
+                <label htmlFor="Sem hotel">
+                  <input type="radio" id="sem-hotel" name="hotel" value="0" onChange={onBookingChange} />
+                  <div>
+                    <span>Sem Hotel</span>
+                    <span>+ R$ 0</span>
+                  </div>
+                </label>
+                <label htmlFor="Com Hotel">
+                  <input type="radio" id="com-hotel" name="hotel" value="350" onChange={onBookingChange} />
+                  <div>
+                    <span>Com Hotel</span>
+                    <span>+ R$ 350</span>
+                  </div>
+                </label>
+              </RadioInputDiv>
+            </TicketSelectionDiv>
+          )} </>
+        : <TicketSelectionDiv>
           <h2>Ingresso escolhido</h2>
           <ChosenTicketDiv>
-            <span>{type}</span>
-            <span>R$ {value}</span>
+            <span>{ticketType} + {bookingType} </span>
+            <span>R$ {ticketValue + bookingValue}</span>
           </ChosenTicketDiv>
         </TicketSelectionDiv>
       }
-      {showBookTicketButton 
-        ?<TicketSelectionDiv>
-          <h2>Fechado! O total ficou em <b>R$ {value}.</b> Agora é só confirmar:</h2>
+      {showBookTicketButton
+        ? <TicketSelectionDiv>
+          <h2>Fechado! O total ficou em <b>R$ {ticketValue + bookingValue}.</b> Agora é só confirmar:</h2>
           <StyledButton type="submit">RESERVAR INGRESSO</StyledButton>
-        </TicketSelectionDiv> 
+        </TicketSelectionDiv>
         : <></>
       }
     </form>
   );
-}
-
+};
