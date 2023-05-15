9c773ed79045f39592b99c0491726ef24356e854
DFCruzz <diegofcruzz@gmail.com>
2023-05-15 16:31:02 -0300

    chore: styled confirmed payment card added

---
+++ src/components/CreditCardComponent/ConfirmedPayment.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ConfirmedPaymentCard = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    h2{
+        margin: 0 15px;
+    }
+
+    div {
+        display: flex;
+        align-items: center;
+        margin-bottom: 12px;
+    }
+`;
