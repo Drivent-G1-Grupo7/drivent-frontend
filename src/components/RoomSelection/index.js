01078fd27058c566a6d4c787246a35142785a15d
duanzin <lucaduan@gmail.com>
2023-05-18 20:42:23 -0300

    feat: check if ticket is payed or online

--- src/components/RoomSelection/index.js
+++ src/components/RoomSelection/index.js
@@ -1,8 +1,5 @@
 import { RoomOptions } from './RoomOption';
 import { RoomBox, RoomSelectionWrapper } from './RoomSelectionWrapper';
-import hotel1Img from '../../assets/images/hotel1.png';
-import hotel2Img from '../../assets/images/hotel2.png';
-import hotel3Img from '../../assets/images/hotel3.png';
 import { useState, useEffect } from 'react';
 
 export default function RoomSelection({ lastSelectedHotel }) {
--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -2,10 +2,29 @@ import { useState, useEffect } from 'react';
 import { Typography } from '@material-ui/core';
 import styled from 'styled-components';
 import HotelSelection from '../../../components/HotelSelection';
+import useTicket from '../../../hooks/api/useTicket';
+import useTicketType from '../../../hooks/api/useTicketType';
 
 export default function Hotel() {
-  const [hasOnlineTicketType, setHasOnlineTicketType] = useState(true);
-  const [hasPayed, setHasPayed] = useState(true);
+  const [hasOnlineTicketType, setHasOnlineTicketType] = useState(false);
+  const [hasPayed, setHasPayed] = useState(false);
+  const { getTicket } = useTicket();
+  const { getTicketTypes } = useTicketType();
+
+  useEffect(() => {  
+    const fetchData = async() => {
+      const data = await getTicket();
+      if (data.status === 'PAID') {
+        setHasPayed(true);
+        const types = await getTicketTypes();
+        const datatype = types.find((type) => {
+          return type.id === data.ticketTypeId;
+        });
+        if (datatype.isRemote === false || datatype.includesHotel === true ) setHasOnlineTicketType(true);
+      };
+    };
+    fetchData().catch();
+  }, []);
 
   return (
     <>
